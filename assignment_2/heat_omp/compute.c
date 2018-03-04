f305bdde19911e1507d7e6d42037f79a1a3dc6a2
Tim Visser <mail@timvisser.nl>
2018-03-04 18:43:12 +0100

    add heat_omp code

--- assignment_2/heat_omp/compute.c
+++ assignment_2/heat_omp/compute.c
@@ -1,23 +1,15 @@
 #include <sys/time.h>
 #include <math.h>
 #include <stdlib.h>
-
 #include "compute.h"
+#include <omp.h>
+#include <stdio.h>
 
-/* ... */
+/// Replace/erase the following line:
+#include "ref1.c"
 
 void do_compute(const struct parameters* p, struct results *r)
 {
-    /* ... */
-
-    struct timeval before, after;
-    gettimeofday(&before, NULL);
-
-    /* ... */
-
-    gettimeofday(&after, NULL);
-    r->time = (double)(after.tv_sec - before.tv_sec) + 
-        (double)(after.tv_usec - before.tv_usec) / 1e6;
-
-    /* ... */
+//omp_set_num_threads(p->nthreads);
+#include "ref2.c"
 }
---
+++ assignment_2/heat_omp/runtests.sh
@@ -0,0 +1,62 @@
+echo "9 thread:"
+./heat_seq -n 100 -m 100 -i 100 -c n100m100.pgm -t n100m100.pgm -p 9| tail -n 1
+./heat_seq -n 1000 -m 1000 -i 100 -c n1000m1000.pgm -t n1000m1000.pgm -p 9| tail -n 1
+./heat_seq -n 5000 -m 5000 -i 100 -c n5000m5000.pgm -t n5000m5000.pgm -p 9| tail -n 1
+./heat_seq -n 20000 -m 100 -i 100 -c n20000m100.pgm -t n20000m100.pgm -p 9| tail -n 1
+./heat_seq -n 100 -m 20000 -i 100 -c n100m20000.pgm -t n100m20000.pgm -p 9| tail -n 1
+
+echo "10 threads:"
+./heat_seq -n 100 -m 100 -i 100 -c n100m100.pgm -t n100m100.pgm -p 10| tail -n 1
+./heat_seq -n 1000 -m 1000 -i 100 -c n1000m1000.pgm -t n1000m1000.pgm -p 10| tail -n 1
+./heat_seq -n 5000 -m 5000 -i 100 -c n5000m5000.pgm -t n5000m5000.pgm -p 10| tail -n 1
+./heat_seq -n 20000 -m 100 -i 100 -c n20000m100.pgm -t n20000m100.pgm -p 10| tail -n 1
+./heat_seq -n 100 -m 20000 -i 100 -c n100m20000.pgm -t n100m20000.pgm -p 10| tail -n 1
+
+echo "11 threads:"
+./heat_seq -n 100 -m 100 -i 100 -c n100m100.pgm -t n100m100.pgm -p 11| tail -n 1
+./heat_seq -n 1000 -m 1000 -i 100 -c n1000m1000.pgm -t n1000m1000.pgm -p 11| tail -n 1
+./heat_seq -n 5000 -m 5000 -i 100 -c n5000m5000.pgm -t n5000m5000.pgm -p 11| tail -n 1
+./heat_seq -n 20000 -m 100 -i 100 -c n20000m100.pgm -t n20000m100.pgm -p 11| tail -n 1
+./heat_seq -n 100 -m 20000 -i 100 -c n100m20000.pgm -t n100m20000.pgm -p 11| tail -n 1
+
+echo "12 threads:"
+./heat_seq -n 100 -m 100 -i 100 -c n100m100.pgm -t n100m100.pgm -p 12| tail -n 1
+./heat_seq -n 1000 -m 1000 -i 100 -c n1000m1000.pgm -t n1000m1000.pgm -p 12| tail -n 1
+./heat_seq -n 5000 -m 5000 -i 100 -c n5000m5000.pgm -t n5000m5000.pgm -p 12| tail -n 1
+./heat_seq -n 20000 -m 100 -i 100 -c n20000m100.pgm -t n20000m100.pgm -p 12| tail -n 1
+./heat_seq -n 100 -m 20000 -i 100 -c n100m20000.pgm -t n100m20000.pgm -p 12| tail -n 1
+
+echo "13 threads:"
+./heat_seq -n 100 -m 100 -i 100 -c n100m100.pgm -t n100m100.pgm -p 13| tail -n 1
+./heat_seq -n 1000 -m 1000 -i 100 -c n1000m1000.pgm -t n1000m1000.pgm -p 13| tail -n 1
+./heat_seq -n 5000 -m 5000 -i 100 -c n5000m5000.pgm -t n5000m5000.pgm -p 13| tail -n 1
+./heat_seq -n 20000 -m 100 -i 100 -c n20000m100.pgm -t n20000m100.pgm -p 13| tail -n 1
+./heat_seq -n 100 -m 20000 -i 100 -c n100m20000.pgm -t n100m20000.pgm -p 13| tail -n 1
+
+echo "14 threads:"
+./heat_seq -n 100 -m 100 -i 100 -c n100m100.pgm -t n100m100.pgm -p 14| tail -n 1
+./heat_seq -n 1000 -m 1000 -i 100 -c n1000m1000.pgm -t n1000m1000.pgm -p 14| tail -n 1
+./heat_seq -n 5000 -m 5000 -i 100 -c n5000m5000.pgm -t n5000m5000.pgm -p 14| tail -n 1
+./heat_seq -n 20000 -m 100 -i 100 -c n20000m100.pgm -t n20000m100.pgm -p 14| tail -n 1
+./heat_seq -n 100 -m 20000 -i 100 -c n100m20000.pgm -t n100m20000.pgm -p 14| tail -n 1
+
+echo "15 threads:"
+./heat_seq -n 100 -m 100 -i 100 -c n100m100.pgm -t n100m100.pgm -p 15| tail -n 1
+./heat_seq -n 1000 -m 1000 -i 100 -c n1000m1000.pgm -t n1000m1000.pgm -p 15| tail -n 1
+./heat_seq -n 5000 -m 5000 -i 100 -c n5000m5000.pgm -t n5000m5000.pgm -p 15| tail -n 1
+./heat_seq -n 20000 -m 100 -i 100 -c n20000m100.pgm -t n20000m100.pgm -p 15| tail -n 1
+./heat_seq -n 100 -m 20000 -i 100 -c n100m20000.pgm -t n100m20000.pgm -p 15| tail -n 1
+
+echo "16 threads:"
+./heat_seq -n 100 -m 100 -i 100 -c n100m100.pgm -t n100m100.pgm -p 16| tail -n 1
+./heat_seq -n 1000 -m 1000 -i 100 -c n1000m1000.pgm -t n1000m1000.pgm -p 16| tail -n 1
+./heat_seq -n 5000 -m 5000 -i 100 -c n5000m5000.pgm -t n5000m5000.pgm -p 16| tail -n 1
+./heat_seq -n 20000 -m 100 -i 100 -c n20000m100.pgm -t n20000m100.pgm -p 16| tail -n 1
+./heat_seq -n 100 -m 20000 -i 100 -c n100m20000.pgm -t n100m20000.pgm -p 16| tail -n 1
+
+echo "32 threads:"
+./heat_seq -n 100 -m 100 -i 100 -c n100m100.pgm -t n100m100.pgm -p 32| tail -n 1
+./heat_seq -n 1000 -m 1000 -i 100 -c n1000m1000.pgm -t n1000m1000.pgm -p 32| tail -n 1
+./heat_seq -n 5000 -m 5000 -i 100 -c n5000m5000.pgm -t n5000m5000.pgm -p 32| tail -n 1
+./heat_seq -n 20000 -m 100 -i 100 -c n20000m100.pgm -t n20000m100.pgm -p 32| tail -n 1
+./heat_seq -n 100 -m 20000 -i 100 -c n100m20000.pgm -t n100m20000.pgm -p 32| tail -n 1
